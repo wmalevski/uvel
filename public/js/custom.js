8d96941b3292ea4bdf8096d5cf33d6632502abfb
Daniel Radoslavov Tonev <dani.tonev@rubberduck.xyz>
2018-01-17 14:01:44 +0200

    Ajax call is okay

--- public/js/custom.js
+++ public/js/custom.js
@@ -94,22 +94,7 @@ $(document).ready(function() {
 
             var ajaxUrl = url + urlAction;
             console.log(collectionData);
-       
-        //    var xhttp = new XMLHttpRequest();
-
-        //    xhttp.open("POST", url + urlAction, true);
-
-        //    xhttp.onreadystatechange = function () {
-
-        //        if (this.readyState == 4 && this.status == 200) {
-
-        //            let data = JSON.parse(this.responseText);
-        //            console.log(data);
-        //        }
-        //    };
-
-        //    xhttp.setRequestHeader("Content-Type", "application/json");
-        //    xhttp.send(JSON.stringify(collectionData));
+    
             ajaxFn('POST', ajaxUrl, handleResponse, collectionData);
         });
     }
