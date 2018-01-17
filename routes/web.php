2adca7cfe299b8a05d72345bad8d7c4d466456e9
criting <galabin@rubberduck.xyz>
2018-01-17 09:58:46 +0200

    Fixing URLS

--- routes/web.php
+++ routes/web.php
@@ -22,53 +22,51 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin', 'middleware' => ['role:superadministrator|administrator']], function() {
     Route::get('/', 'Dashboardcontroller@index');
 
-    Route::get('/stones/sizes', 'StoneSizesController@index');
+    Route::get('/stones/sizes', 'StoneSizesController@index')->name('sizes');
     Route::post('/stones/sizes', 'StoneSizesController@store');
 
-    Route::get('/stones/styles', 'StoneStylesController@index');
+    Route::get('/stones/styles', 'StoneStylesController@index')->name('styles');
     Route::post('/stones/styles', 'StoneStylesController@store');
 
-    Route::get('/stones/contours', 'StoneContoursController@index');
+    Route::get('/stones/contours', 'StoneContoursController@index')->name('contours');
     Route::post('/stones/contours', 'StoneContoursController@store');
 
-    Route::get('/stones', 'StonesController@index');
+    Route::get('/stones', 'StonesController@index')->name('stones');
     Route::post('/stones', 'StonesController@store');
 
-    Route::get('/stores', 'StoresController@index');
+    Route::get('/stores', 'StoresController@index')->name('stores');
     Route::post('/stores', 'StoresController@store');
 
     Route::get('/stores/{store}', 'StoresController@edit');
     Route::post('/stores/{store}', 'StoresController@update');
 
-    Route::get('/nomenclatures', 'NomenclaturesController@index');
+    Route::get('/nomenclatures', 'NomenclaturesController@index')->name('nomenclatures');
     Route::post('/nomenclatures', 'NomenclaturesController@store');
 
-    Route::get('/materials', 'MaterialsController@index');
+    Route::get('/materials', 'MaterialsController@index')->name('materials');
     Route::post('/materials', 'MaterialsController@store');
 
     Route::get('/materials/{material}', 'MaterialsController@edit');
     Route::post('/materials/{material}', 'MaterialsController@update');
 
-    Route::get('/prices', 'PricesController@index');
+    Route::get('/prices', 'PricesController@index')->name('prices');
     Route::post('/prices', 'PricesController@index');
 
     Route::get('/prices/{material}', 'PricesController@show');
-    Route::post('/prices/{material}', 'PricesController@store')->name('prices');
+    Route::post('/prices/{material}', 'PricesController@store');
 
-    Route::get('/jewels', 'JewelsController@index');
+    Route::get('/jewels', 'JewelsController@index')->name('jewels');
     Route::post('/jewels', 'JewelsController@store');
 
     Route::get('/jewels/{jewel}', 'JewelsController@edit');
     Route::post('/jewels/{jewel}', 'JewelsController@update');
 
-    Route::get('/models', 'ModelsController@index');
+    Route::get('/models', 'ModelsController@index')->name('models');
     Route::post('/models', 'ModelsController@store');
 
     Route::get('/models/{model}', 'ModelsController@edit');
     Route::post('/models/{model}', 'ModelsController@update');
 
-    Route::get('/products', 'ProductsController@index');
+    Route::get('/products', 'ProductsController@index')->name('products');
     Route::post('/products', 'ProductsController@store');
-
-    
 });
