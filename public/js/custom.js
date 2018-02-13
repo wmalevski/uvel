3aa2b8036369349854d2e63ce0e3a4c50b25dc58
criting <galabin@rubberduck.xyz>
2018-02-13 13:38:57 +0200

    Adding jQuery image upload

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-
     $('select').select2();
 
     var max_fields      = 10; 
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -67,7 +67,7 @@
 
   </style>
   <link href="{{ URL::asset('style.css') }}" rel="stylesheet">
-  <link href="{{ URL::asset('css/dropzone.css') }}" rel="stylesheet">
+  <link href="{{ URL::asset('jquery.fileupload.css') }}" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 </head>
 
@@ -607,7 +607,128 @@
   <script type="text/javascript" src="{{ URL::asset('vendor.js') }}"></script>
   <script type="text/javascript" src="{{ URL::asset('bundle.js') }}"></script>
   <script type="text/javascript" src="{{ URL::asset('js/custom.js') }}"></script>
-  <script type="text/javascript" src="{{ URL::asset('js/dropzone.js') }}"></script>
+  
+
+
+  <script src="{{ URL::asset('js/jquery.ui.widget.js') }}"></script>
+  <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+  <script src="https://blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
+  <!-- The Canvas to Blob plugin is included for image resizing functionality -->
+  <script src="https://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+  <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+  <script src="{{ URL::asset('js/jquery.iframe-transport.js') }}"></script>
+  <!-- The basic File Upload plugin -->
+  <script src="{{ URL::asset('js/jquery.fileupload.js') }}"></script>
+  <!-- The File Upload processing plugin -->
+  <script src="{{ URL::asset('js/jquery.fileupload-process.js') }}"></script>
+  <!-- The File Upload image preview & resize plugin -->
+  <script src="{{ URL::asset('js/jquery.fileupload-image.js') }}"></script>
+  <!-- The File Upload audio preview plugin -->
+  <script src="{{ URL::asset('js/jquery.fileupload-audio.js') }}"></script>
+  <!-- The File Upload video preview plugin -->
+  <script src="{{ URL::asset('js/jquery.fileupload-video.js') }}"></script>
+  <!-- The File Upload validation plugin -->
+  <script src="{{ URL::asset('js/jquery.fileupload-validate.js') }}"></script>
+
+  <script>
+      /*jslint unparam: true, regexp: true */
+      /*global window, $ */
+      $(function () {
+          'use strict';
+          // Change this to the location of your server-side upload handler:
+          var url = window.location.hostname === 'blueimp.github.io' ?
+                      '//jquery-file-upload.appspot.com/' : 'server/php/',
+              uploadButton = $('<button/>')
+                  .addClass('btn btn-primary')
+                  .prop('disabled', true)
+                  .text('Processing...')
+                  .on('click', function () {
+                      var $this = $(this),
+                          data = $this.data();
+                      $this
+                          .off('click')
+                          .text('Abort')
+                          .on('click', function () {
+                              $this.remove();
+                              data.abort();
+                          });
+                      data.submit().always(function () {
+                          $this.remove();
+                      });
+                  });
+          $('#fileupload').fileupload({
+              url: url,
+              dataType: 'json',
+              autoUpload: false,
+              acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+              maxFileSize: 999000,
+              // Enable image resizing, except for Android and Opera,
+              // which actually support image resizing, but fail to
+              // send Blob objects via XHR requests:
+              disableImageResize: /Android(?!.*Chrome)|Opera/
+                  .test(window.navigator.userAgent),
+              previewMaxWidth: 100,
+              previewMaxHeight: 100,
+              previewCrop: true
+          }).on('fileuploadadd', function (e, data) {
+              data.context = $('<span/>').appendTo('#files');
+              $.each(data.files, function (index, file) {
+                  var node = $('<p/>');
+                  if (!index) {
+                      node;
+                  }
+                  node.appendTo(data.context);
+              });
+          }).on('fileuploadprocessalways', function (e, data) {
+              var index = data.index,
+                  file = data.files[index],
+                  node = $(data.context.children()[index]);
+              if (file.preview) {
+                  node
+                      .prepend(file.preview);
+              }
+              if (file.error) {
+                  node
+                      .append('<br>')
+                      .append($('<span class="text-danger"/>').text(file.error));
+              }
+              if (index + 1 === data.files.length) {
+                  data.context.find('button')
+                      .text('Upload')
+                      .prop('disabled', !!data.files.error);
+              }
+          }).on('fileuploadprogressall', function (e, data) {
+              var progress = parseInt(data.loaded / data.total * 100, 10);
+              $('#progress .progress-bar').css(
+                  'width',
+                  progress + '%'
+              );
+          }).on('fileuploaddone', function (e, data) {
+              $.each(data.result.files, function (index, file) {
+                  if (file.url) {
+                      var link = $('<a>')
+                          .attr('target', '_blank')
+                          .prop('href', file.url);
+                      $(data.context.children()[index])
+                          .wrap(link);
+                  } else if (file.error) {
+                      var error = $('<span class="text-danger"/>').text(file.error);
+                      $(data.context.children()[index])
+                          .append('<br>')
+                          .append(error);
+                  }
+              });
+          }).on('fileuploadfail', function (e, data) {
+              $.each(data.files, function (index) {
+                  var error = $('<span class="text-danger"/>').text('File upload failed.');
+                  $(data.context.children()[index])
+                      .append('<br>')
+                      .append(error);
+              });
+          }).prop('disabled', !$.support.fileInput)
+              .parent().addClass($.support.fileInput ? undefined : 'disabled');
+      });
+      </script>
 </body>
 
 </html>
