520b1d987dc30396a4db9267d8d094c921edc3d1
criting <galabin@rubberduck.xyz>
2018-03-29 16:44:08 +0300

    Adding print icons to tables

--- resources/views/admin/discounts/table.blade.php
+++ resources/views/admin/discounts/table.blade.php
@@ -7,6 +7,6 @@
     <td>@if($discount->user) {{ App\User::find($discount->user)->name }} @endif</td>
     <td>
         <a href="discounts/{{$discount->id}}" class="edit-btn" data-toggle="modal" data-target="#editDiscount"><i class="c-brown-500 ti-pencil"></i></a>
-        <a href="discounts/print/{{$product->id}}" class="edit-btn"><i class="c-brown-500 ti-printer"></i></a> 
+        <a href="discounts/print/{{$discount->id}}" class="edit-btn"><i class="c-brown-500 ti-printer"></i></a> 
     </td>
 </tr>
