4bad7d188dbb01aeebc705de45a6827d2f64a6a4
criting <galabin@rubberduck.xyz>
2018-04-17 10:53:02 +0300

    Working on Jewels

--- resources/views/admin/jewels/index.blade.php
+++ resources/views/admin/jewels/index.blade.php
@@ -44,6 +44,32 @@ aria-hidden="true">
     </div>
 </div>
 
+<div class="modal fade" id="editJewel" role="dialog" aria-labelledby="editJewelLabel"
+aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editJewelLabel">Редактиране на модел</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="info-cont">
+                </div>
+                {{ csrf_field() }}
+
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Затвори</button>
+                <button type="submit" id="edit" class="btn btn-primary">Обнови</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="row">
   <div class="col-md-12">
     <div class="bgc-white bd bdrs-3 p-20 mB-20">
