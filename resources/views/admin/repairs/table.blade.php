dbfd2da0b83ecbeaf59e50ef8912551e0a45de3d
criting <galabin@rubberduck.xyz>
2018-04-23 09:38:42 +0300

    Edit on stones

--- resources/views/admin/repairs/table.blade.php
+++ resources/views/admin/repairs/table.blade.php
@@ -7,7 +7,7 @@
     <td>@if($repair->status == 'repairing') Приет @elseif($repair->status == 'done') Готов @else Върнат  @endif</td> 
 
     <td> @if($repair->status == 'repairing') 
-        <span data-url="repairs/{{$repair->id}}" class="edit-btn" data-toggle="modal" data-target="#fullEditRepair"><i class="c-brown-500 ti-pencil"></i></span> @endif @if($repair->status == 'done') <a href="repairs/return/{{$repair->id}}" class="edit-btn" data-toggle="modal" data-target="#returnRepair"><i class="c-brown-500 ti-reload"></i></a> @endif
+        <span data-url="repairs/{{$repair->id}}" class="edit-btn" data-toggle="modal" data-target="#fullEditRepair"><i class="c-brown-500 ti-pencil"></i></span> @endif @if($repair->status == 'done') <span href="repairs/return/{{$repair->id}}" class="edit-btn" data-toggle="modal" data-target="#returnRepair"><i class="c-brown-500 ti-reload"></i></span> @endif
         <a href="repairs/{{$repair->id}}" class="edit-btn" data-toggle="modal" data-target="#fullEditRepair"><i class="c-brown-500 ti-info-alt"></i></a>
         <a href="repairs/certificate/{{$repair->id}}" class="edit-btn"><i class="c-brown-500 ti-printer"></i></a> 
         <a href="repairs/delete/{{$repair->id}}" class="delete-btn"><i class="c-brown-500 ti-trash"></i></a>
