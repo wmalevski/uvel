7226388b20832ca292ccd179c3376e8701115ad5
criting <galabin@rubberduck.xyz>
2018-04-26 10:05:59 +0300

    showing photos

--- resources/views/admin/stones/edit.blade.php
+++ resources/views/admin/stones/edit.blade.php
@@ -84,7 +84,11 @@
                 <label class="button" for="fileElem-edit">Select some files</label>
                 <div class="drop-area-gallery"></div>
             </div>
-          
+          @foreach($stone_photos as $photo)
+            <div class="col-md-3">
+                <img src="{{ asset("uploads/stones/" . $photo->photo) }}" alt="" class="img-responsive" />
+            </div>
+          @endforeach
         </div>
 
         <div class="modal-footer">
