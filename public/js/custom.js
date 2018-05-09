cefb49a8cd80d1f391cd7c9cc1f49ba936357cdc
Andriana Badeva <andriana.badeva@rubberduck.xyz>
2018-05-09 16:00:04 +0300

    working on handle response post old version

--- public/js/custom.js
+++ public/js/custom.js
@@ -1096,100 +1096,99 @@ var uvel,
 
      
       function handleResponsePost(response, elements, currentPressedBtn) {
-        var alertAreas = document.getElementsByClassName('info-cont');
+        
+        var responseHolder = document.forms[nameForm].firstElementChild.firstElementChild;
 
-        Array.from(alertAreas).forEach(function(responseHolder) {
-            responseHolder.innerHTML = '';
+        responseHolder.innerHTML = '';
 
-            if (response.hasOwnProperty('errors')) {
+        if (response.hasOwnProperty('errors')) {
 
-              var holder = document.createDocumentFragment();
-              var errors = response.errors;
+          var holder = document.createDocumentFragment();
+          var errors = response.errors;
 
-              for (var err in errors) {
-                var collectionErr = errors[err];
+          for (var err in errors) {
+            var collectionErr = errors[err];
 
-                collectionErr.forEach(function (msg) {
-                  var errorContainer = document.createElement('div');
-                  errorContainer.innerText = msg;
-                  errorContainer.className = 'alert alert-danger';
-                  holder.appendChild(errorContainer);
-                });
-              }
+            collectionErr.forEach(function (msg) {
+              var errorContainer = document.createElement('div');
+              errorContainer.innerText = msg;
+              errorContainer.className = 'alert alert-danger';
+              holder.appendChild(errorContainer);
+            });
+          }
 
-              responseHolder.appendChild(holder);
+          responseHolder.appendChild(holder);
+
+        } else {
+
+            var successContainer = document.createElement('div');
+              successContainer.innerText = 'Успешно добавихте';
+              successContainer.className = 'alert alert-success';
+
+            responseHolder.appendChild(successContainer);
+
+            if (nameForm === 'addPrice') {
+
+              var select = collectionSelects[0];
+              var tableId = document.querySelector('#' + select.options[select.selectedIndex].value + ' tbody');
+
+              tableId.innerHTML += response.success;
 
             } else {
 
-                var successContainer = document.createElement('div');
-                  successContainer.innerText = 'Успешно добавихте';
-                  successContainer.className = 'alert alert-success';
+              if(nameForm === 'addRepair') {
+              var repairId = response.id,
+                  certificateButton = document.querySelector('button#certificate');
 
-                responseHolder.appendChild(successContainer);
+              certificateButton.dataset.repairId = repairId;
+              certificateButton.disabled = false;
 
-                if (nameForm === 'addPrice') {
-
-                  var select = collectionSelects[0];
-                  var tableId = document.querySelector('#' + select.options[select.selectedIndex].value + ' tbody');
-
-                  tableId.innerHTML += response.success;
-
-                } else {
-
-                  if(nameForm === 'addRepair') {
-                  var repairId = response.id,
-                      certificateButton = document.querySelector('button#certificate');
-
-                  certificateButton.dataset.repairId = repairId;
-                  certificateButton.disabled = false;
-
-                }
-
-                var tableBody = document.querySelector('table.table tbody');
-
-                tableBody.innerHTML += response.success;
-              }
-
-
-              elements.forEach(function (el) {
-
-                var elType = el.getAttribute('type');
-
-                if (typeof el != null && elType !== 'hidden' && typeof(el.dataset.clear) == 'undefined') {
-                  if(elType == 'checkbox') {
-                    el.checked = false;
-                  }
-
-                  if(el.tagName == 'SELECT') {
-                    $(el).val(null).trigger('change');
-                  }
-
-                  el.value = '';
-
-                  if(elType == 'file'){
-
-                    $(el).parent().find('drop-area-input').val('');
-
-                    $(el).val('');
-
-                    var gallery = $(el).parent().children('.drop-area-gallery');
-                    gallery.html('');
-                          
-                  }     
-                  
-
-                }
-
-              })
             }
 
-          });
+            var tableBody = document.querySelector('table.table tbody');
 
-        ////
+            tableBody.innerHTML += response.success;
+          }
+
+
+          elements.forEach(function (el) {
+
+            var elType = el.getAttribute('type');
+
+            if (typeof el != null && elType !== 'hidden' && typeof(el.dataset.clear) == 'undefined') {
+              if(elType == 'checkbox') {
+                el.checked = false;
+              }
+
+              if(el.tagName == 'SELECT') {
+                $(el).val(null).trigger('change');
+              }
+
+              el.value = '';
+
+              if(elType == 'file'){
+
+                $(el).parent().find('drop-area-input').val('');
+
+                $(el).val('');
+
+                var gallery = $(el).parent().children('.drop-area-gallery');
+                gallery.html('');
+                      
+              }     
+              
+
+            }
+
+          })
+        }
 
         editAction();
 
         pendingRequest = false;
+
+
+
 
       }
 
