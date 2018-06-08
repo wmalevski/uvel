772a83c0e2e473581e7b82f21ba846255ef8e678
criting <galabin@rubberduck.xyz>
2018-06-08 16:59:02 +0300

    Adding barcode and date of creating in the certificate, and also in the table

--- resources/views/admin/repairs/table.blade.php
+++ resources/views/admin/repairs/table.blade.php
@@ -5,7 +5,7 @@
     <td>{{ $repair->customer_phone }}</td> 
     <td>{{ App\Repair_types::withTrashed()->find($repair->type)->name }}</td> 
     <td>@if($repair->status == 'repairing') Приет @elseif($repair->status == 'done') Готов @else Върнат  @endif</td> 
-
+    <td>{{ $repair->created_at }}</td>
     <td> @if($repair->status == 'repairing') 
         <span data-url="repairs/{{$repair->barcode}}" class="edit-btn" data-toggle="modal" data-target="#fullEditRepair"><i class="c-brown-500 ti-pencil"></i></span> @endif @if($repair->status == 'done') <span href="repairs/return/{{$repair->barcode}}" class="edit-btn" data-toggle="modal" data-target="#returnRepair"><i class="c-brown-500 ti-reload"></i></span> @endif
         <a href="repairs/certificate/{{$repair->id}}" class="print-btn"><i class="c-brown-500 ti-printer"></i></a> 
