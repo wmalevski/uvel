0e49d7873efd932d085ef2d1fcfba3c0fc5d4e18
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2018-06-11 17:24:56 +0300

    Fix the html of the models table and adds the sorting table class

--- public/js/table-sort.js
+++ public/js/table-sort.js
@@ -2,8 +2,8 @@ $(function() {
     $(".tablesort").tablesorter({
     
         headers: {
-          // disable sorting of the first & second column - before we would have to had made two entries
-          // note that "first-name" is a class on the span INSIDE the first column th cell
+          // disable sorting of the specific column
+          // note that "sort-false" is a class on the span INSIDE the first column th cell
           '.sort-false' : {
             // disable it by setting the property sorter to false
             sorter: false
