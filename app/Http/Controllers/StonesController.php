6c91d7a1739d3bd6ef3de871c05c1a4744e1c998
criting <galabin@rubberduck.xyz>
2018-06-13 09:50:13 +0300

    Hide after third number for price

--- app/Http/Controllers/StonesController.php
+++ app/Http/Controllers/StonesController.php
@@ -60,7 +60,7 @@ class StonesController extends Controller
             'size' => 'required|numeric',
             'style' => 'required',
             'contour' => 'required',
-            'price' => 'required|numeric|between:0.1,100000',
+            'price' => 'required|numeric|regex:/^\d*(\.\d{1,3})?$/',
             'amount' => 'required|numeric|between:0.01,100000'
          ]);
 
@@ -143,7 +143,7 @@ class StonesController extends Controller
             'size' => 'required|numeric',
             'style' => 'required',
             'contour' => 'required',
-            'price' => 'required|numeric|regex:/^\d*(\.\d{1,2})?$/',
+            'price' => 'required|numeric|regex:/^\d*(\.\d{1,3})?$/',
             'amount' => 'required|numeric|between:0.01,100000'
          ]);
 
