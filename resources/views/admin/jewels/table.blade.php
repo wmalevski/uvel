a7f2e3886a513392fe9f075c656fdb93ab7797e2
criting <galabin@rubberduck.xyz>
2018-06-19 10:41:56 +0300

    Fix trying to get property of non object

--- resources/views/admin/jewels/table.blade.php
+++ resources/views/admin/jewels/table.blade.php
@@ -1,6 +1,6 @@
 <tr data-id="{{ $jewel->id }}">
     <td>{{ $jewel->name }}</td> 
-    <td>{{ App\Materials_type::withTrashed()->find($jewel->material)->name }} - {{ App\Materials::withTrashed()->find($jewel->material)->color }} - {{ App\Materials::withTrashed()->find(App\Materials::withTrashed()->find($jewel->material)->parent)->code }}</td> 
+    <td>{{ App\Materials_type::withTrashed()->find(App\Materials::withTrashed()->find($jewel->material)->parent)->name }} - {{ App\Materials::withTrashed()->find($jewel->material)->color }} - {{ App\Materials::withTrashed()->find(App\Materials::withTrashed()->find($jewel->material)->parent)->code }}</td> 
     <td><span data-url="jewels/{{$jewel->id}}" class="edit-btn" data-toggle="modal" data-target="#editJewel"><i class="c-brown-500 ti-pencil"></i></span>
         <span data-url="jewels/delete/{{$jewel->id}}" class="delete-btn"><i class="c-brown-500 ti-trash"></i></span>
     </td>
