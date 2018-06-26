83aa6e7da5f99dde51646f91498e56e7b9ba425b
criting <galabin@rubberduck.xyz>
2018-06-26 16:46:35 +0300

    Model options db

---
+++ app/ModelOptions.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelOptions extends Model
+{
+    use SoftDeletes;
+    
+        protected $fillable = [
+            'model',
+            'material',
+            'retail_price',
+            'wholesale_price',
+            'default'
+        ];
+    
+        protected $table = 'model_options';
+        protected $dates = ['deleted_at'];    
+}
---
+++ database/migrations/2018_06_26_163215_create_model_options_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModelOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('model_options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('model')->references('id')->on('models');
+            $table->integer('material')->references('id')->on('materials');
+            $table->integer('retail_price')->references('id')->on('prices');
+            $table->integer('wholesale_price')->references('id')->on('prices');
+            $table->enum('default', ['yes', 'no'])->default('no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('model_options');
+    }
+}
