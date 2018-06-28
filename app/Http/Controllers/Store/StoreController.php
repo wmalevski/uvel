c58f0bca60f28c289f8f1091a951f05d87cb5830
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2018-06-28 17:09:04 +0300

    Feature Store - creates Store controllers folder

---
+++ app/Http/Controllers/Store/StoreController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+}
