f4931c12cb1f4ea5aa48a7d8b1501ab2704ee4da
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2018-07-12 14:05:35 +0300

    Change the name of the Currency Model

--- app/Currency.php
+++ app/Currency.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Currencies extends Model
+class Currency extends Model
 {
     use SoftDeletes;
     
