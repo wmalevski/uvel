501f6358a3201de0225cd106c9da4f7123802565
criting <galabin@rubberduck.xyz>
2018-07-30 16:35:10 +0300

    Fixing user substitutions with calling relations

--- app/UserSubstitution.php
+++ app/UserSubstitution.php
@@ -11,4 +11,14 @@ class UserSubstitution extends Model
 
     protected $table = 'user_substitutions';
     protected $dates = ['date_from', 'date_to', 'deleted_at'];
+
+    public function store()
+    {
+    	return $this->belongsTo('App\Store');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
