484afc2a82bf5330f267378add6dcc02f7a28dbb
criting <galabin@rubberduck.xyz>
2018-08-01 15:09:38 +0300

    Tooltip information for no price

--- resources/views/admin/materials/table.blade.php
+++ resources/views/admin/materials/table.blade.php
@@ -7,5 +7,12 @@
     <td style="width: 12%;">
         <span data-url="materials/{{$material->id}}" class="edit-btn" data-toggle="modal" data-target="#editMaterial"><i class="c-brown-500 ti-pencil"></i></span>
         <span data-url="materials/delete/{{$material->id}}" class="delete-btn"><i class="c-brown-500 ti-trash"></i></span> 
+        @if(!count($material->quantity))
+            <button type="button" class="btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="Този материал няма добавена наличност"><i class="c-red-500 ti-info-alt"></i></button>
+        @endif
+
+        @if(!count($material->pricesBuy) || !count($material->pricesSell))
+            <button type="button" class="btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="Този материал няма добавени цени"><i class="c-red-500 ti-info-alt"></i></button>
+        @endif
     </td>
 </tr>
