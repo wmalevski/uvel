7dfc8f62c97cdb158494c608bd6a3c649033dad5
ibozhanov <ivan.bozhanov@rubberduck.xyz>
2018-08-02 10:09:39 +0300

    materials quantity refactoring done

--- resources/views/admin/materials_quantity/table.blade.php
+++ resources/views/admin/materials_quantity/table.blade.php
@@ -4,7 +4,7 @@
     <td>{{ App\Stores::withTrashed()->find($material->store)->name }}</td>
     <td>
         {{-- <a href="materials/{{$material->id}}" class="edit-btn" data-toggle="modal" data-target="#editMaterial"><i class="c-brown-500 ti-pencil"></i></a> --}}
-        <span data-url="mquantity/{{$material->id}}" class="edit--popup edit-btn" data-toggle="modal" data-target="#editMQuantity"><i class="c-brown-500 ti-pencil"></i></span>
+        <span data-url="mquantity/{{$material->id}}" class="edit--popup edit-btn" data-form-type="edit" data-form="materialsQuantity" data-toggle="modal" data-target="#editMQuantity"><i class="c-brown-500 ti-pencil"></i></span>
         
         <span data-url="mquantity/delete/{{$material->id}}" class="delete-btn"><i class="c-brown-500 ti-trash"></i></span> 
     </td>
