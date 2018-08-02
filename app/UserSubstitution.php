ea3afcabbb6ac8c7eb5afc566e684f86b9f04e04
criting <galabin@rubberduck.xyz>
2018-08-02 16:22:38 +0300

    Adding withtrashed to store and user in user submissions

--- app/UserSubstitution.php
+++ app/UserSubstitution.php
@@ -14,12 +14,12 @@ class UserSubstitution extends Model
 
     public function store()
     {
-    	return $this->belongsTo('App\Store');
+    	return $this->belongsTo('App\Store')->withTrashed();
     }
 
     public function user()
     {
-    	return $this->belongsTo('App\User');
+    	return $this->belongsTo('App\User')->withTrashed();
     }
 
     public function scopeSubstitution($query)
