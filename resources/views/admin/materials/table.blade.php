08f74e3cfe0642f143206136e6bc831b6f94e29e
criting <galabin@rubberduck.xyz>
2018-08-14 15:50:13 +0300

    Changing tooltips and adding error messages

--- resources/views/admin/materials/table.blade.php
+++ resources/views/admin/materials/table.blade.php
@@ -7,12 +7,21 @@
     <td style="width: 12%;">
         <span data-url="materials/{{$material->id}}" class="edit-btn" data-toggle="modal" data-target="#editMaterial"><i class="c-brown-500 ti-pencil"></i></span>
         <span data-url="materials/delete/{{$material->id}}" class="delete-btn"><i class="c-brown-500 ti-trash"></i></span> 
-        @if(!count($material->quantity))
-            <button type="button" class="btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="Този материал няма добавена наличност"><i class="c-red-500 ti-info-alt"></i></button>
-        @endif
 
-        @if(!count($material->pricesBuy) || !count($material->pricesSell))
-            <button type="button" class="btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="Този материал няма добавени цени"><i class="c-red-500 ti-info-alt"></i></button>
+        @if(!count($material->quantity) || !count($material->pricesBuy) || !count($material->pricesSell) || !$material->stock_price || !$material->stock_price)
+        <button type="button" class="btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="
+            @if(!count($material->quantity))
+                Този материал няма добавена наличност
+            @endif
+
+            @if(!count($material->pricesBuy) || !count($material->pricesSell))
+                Този материал няма добавени цени
+            @endif
+
+            @if(!$material->stock_price)
+                Този материал няма добавена борсова цена
+            @endif
+        "><i class="c-red-500 ti-info-alt"></i></button>
         @endif
     </td>
 </tr>
