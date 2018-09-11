2f565f5e441d87c1313d168a285e3956b3efa520
criting <galabin@rubberduck.xyz>
2018-09-11 15:57:10 +0300

    Fixing custom discount on selling

--- resources/views/admin/selling/table.blade.php
+++ resources/views/admin/selling/table.blade.php
@@ -3,5 +3,5 @@
     <td>{{ $item->quantity }}</td>
     <td>{{ $item->attributes->weight }}</td>
     <td>{{ $item->price }}</td>
-    <td><span data-url="admin/sell/removeItem/{{$item->id}}" class="delete-btn cart"><i class="c-brown-500 ti-trash"></i></span></td>
+    <td><span data-url="ajax/sell/removeItem/{{$item->id}}" class="delete-btn cart"><i class="c-brown-500 ti-trash"></i></span></td>
 </tr>
