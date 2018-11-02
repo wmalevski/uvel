1e4852296984ba8d40ddf4d1385f4eb4b44fe3b2
criting <galabin@rubberduck.xyz>
2018-11-02 17:44:13 +0200

    Final touches on register fields

--- resources/views/store/pages/user/register.blade.php
+++ resources/views/store/pages/user/register.blade.php
@@ -86,11 +86,6 @@
                                         <input name="postcode" id="postcode" class="form-control " type="text">
                                     </li>
 
-                                    <li id="country" class="">
-                                        <label class="control-label" for="country">Държава <span class="req">*</span></label>
-                                        <input name="country" id="country" class="form-control " type="text">
-                                    </li>
-
                                     <li id="phone" class="">
                                         <label class="control-label" for="phone">Телефон <span class="req">*</span></label>
                                         <input name="phone" id="phone" class="form-control " type="tel">
