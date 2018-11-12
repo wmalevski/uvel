a11edc99ceafef1e86597855141b248d5ff05896
criting <galabin@rubberduck.xyz>
2018-11-12 13:03:49 +0200

    Online store home slider

--- resources/views/admin/sliders/index.blade.php
+++ resources/views/admin/sliders/index.blade.php
@@ -20,7 +20,7 @@ aria-hidden="true">
                     {{ csrf_field() }}
 
                     <div class="drop-area" name="add">
-                        <input type="file" name="images" class="drop-area-input" id="fileElem-add" multiple accept="image/*" >
+                        <input type="file" name="images" class="drop-area-input" id="fileElem-add" accept="image/*" >
                         <label class="button" for="fileElem-add">Select some files</label>
                         <div class="drop-area-gallery"></div>
                     </div>
@@ -68,7 +68,7 @@ aria-hidden="true">
 <div class="row">
   <div class="col-md-12">
     <div class="bgc-white bd bdrs-3 p-20 mB-20">
-      <h4 class="c-grey-900 mB-20">Камъни <button class="add-btn btn btn-primary" type="button" id="dropdownMenuButton" data-form-type="add" data-form="sliders" data-toggle="modal" data-target="#addSlide">Добави</button></h4>
+      <h4 class="c-grey-900 mB-20">Слайдове <button class="add-btn btn btn-primary" type="button" id="dropdownMenuButton" data-form-type="add" data-form="sliders" data-toggle="modal" data-target="#addSlide">Добави</button></h4>
       <p>Преглед на слайдове</p>
       <table class="table tablesort">
         <thead>
