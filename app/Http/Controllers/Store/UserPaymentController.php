f4bc8c6a59ef64226dee505d0258744b87e8e093
criting <galabin@rubberduck.xyz>
2018-11-13 15:50:02 +0200

    User payments online store migrations

---
+++ app/Http/Controllers/Store/UserPaymentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Store;
+
+use App\UserPayment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserPaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserPayment  $userPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserPayment $userPayment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserPayment  $userPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserPayment $userPayment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserPayment  $userPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserPayment $userPayment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserPayment  $userPayment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserPayment $userPayment)
+    {
+        //
+    }
+}
