60cd09854240d1c3d39246c314b795400effd689
criting <galabin@rubberduck.xyz>
2018-11-27 11:00:45 +0200

    Creating controller for the Safe, migrations for expenses, expense types and daily reports

---
+++ app/Http/Controllers/DailyReportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DailyReport;
+use Illuminate\Http\Request;
+
+class DailyReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DailyReport  $dailyReport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DailyReport $dailyReport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DailyReport  $dailyReport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DailyReport $dailyReport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DailyReport  $dailyReport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DailyReport $dailyReport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DailyReport  $dailyReport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DailyReport $dailyReport)
+    {
+        //
+    }
+}
