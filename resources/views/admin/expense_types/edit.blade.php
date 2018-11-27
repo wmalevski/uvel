e1434c3ab0ade03bde255527eb09186773a67d54
criting <galabin@rubberduck.xyz>
2018-11-27 11:11:11 +0200

    Working on expense types

---
+++ resources/views/admin/expense_types/edit.blade.php
@@ -0,0 +1,27 @@
+<div class="editModalWrapper">
+    <div class="modal-header">
+        <h5 class="modal-title" id="fullEditRepairLabel">Промени вид разход</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    
+    <form method="POST" data-type="edit" name="expensetypes" action="expensetypes/{{ $type->id }}">
+        <input name="_method" type="hidden" value="PUT">
+        <div class="modal-body">    
+                <div class="info-cont">
+                </div>
+            {{ csrf_field() }}
+    
+            <div class="form-group">
+                <label for="1">Име: </label>
+                <input type="text" class="form-control" value="{{ $type->name }}" id="1" name="name" placeholder="Име:">
+            </div>
+        </div>
+    
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Затвори</button>
+            <button type="submit" id="edit" data-state="edit_state" class="action--state_button edit-btn-modal btn btn-primary">Промени</button>
+        </div>
+    </form>
+</div>
