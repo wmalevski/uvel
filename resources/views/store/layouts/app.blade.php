20219d2888ab1b3bfc01e69229a928f835263849
Borislav Minchev <borislav.minchev@rubberduck.xyz>
2018-12-05 13:39:32 +0200

    initial css

--- resources/views/store/layouts/app.blade.php
+++ resources/views/store/layouts/app.blade.php
@@ -33,6 +33,7 @@
 	<link href="{{ asset('store/stylesheets/cs.media.3x.css') }}" rel="stylesheet" type="text/css" media="all">
 	<link href="{{ asset('store/stylesheets/spr.css') }}" rel="stylesheet" type="text/css" media="all">
 	<link href="{{ asset('store/stylesheets/addthis.css') }}" rel="stylesheet" type="text/css" media="all">
+	<link href="{{ asset('store/stylesheets/custom.css') }}" rel="stylesheet" type="text/css" media="all">
     
 	<!-- JavaScript -->
 	<script src="{{ asset('store/javascripts/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
