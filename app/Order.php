592c2aa19e69986a6f5efb9fc4f0937007dca275
criting <galabin@rubberduck.xyz>
2018-12-12 16:33:27 +0200

    Making new migrations, models and controllers

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'model_id',
+        'jewel_id',
+        'material_id',
+        'price_id',
+        'weight',
+        'size',
+        'workmanship',
+        'price',
+        'quantity',
+        'comment',
+        'cash_group',
+        'earnest',
+        'status'
+    ];
+
+    protected $table = 'orders';
 }
