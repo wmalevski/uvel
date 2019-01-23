a709108039fcc4cc1476d0c6bc8ff5de49494214
Borislav Minchev <borislav.minchev@rubberduck.xyz>
2019-01-23 16:14:51 +0200

    added proper initializing parameteres for Select2 in Orders create

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -43,7 +43,7 @@
 						<div class="form-group col-md-6">
 							<label>Модел:</label>
 							
-							<select id="model_select" url="ajax/orders/getModelInfo/" name="model_id" class="model-select form-control model-filled" data-calculatePrice-model>
+							<select url="ajax/orders/getModelInfo/" name="model_id" class="model-select form-control model-filled" data-calculatePrice-model>
 								<option value="">Избери</option>
 								
 								@foreach($models as $model)
