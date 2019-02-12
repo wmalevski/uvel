02081ddcb90df6097f8416f251006c1363d2bbea
criting <galabin@rubberduck.xyz>
2019-02-12 13:25:07 +0000

    Hotfix/mailchimp add email

--- resources/views/admin/mailchimp/index.blade.php
+++ resources/views/admin/mailchimp/index.blade.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Добавяне на Subscriber</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Добавяне на абонат</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -17,8 +17,8 @@
                     {{ csrf_field() }}
                     <div class="form-row">
                         <div class="form-group col-md-12">
-                            <label for="1">Имейл</label>
-                            <input type="text" class="form-control" id="1" name="name" placeholder="Имейл адрес…">
+                            <label for="email">Имейл</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Имейл адрес…">
                         </div>
                     </div>
                 </div>
@@ -42,7 +42,7 @@
   <div class="col-md-12">
     <div class="bgc-white bd bdrs-3 p-20 mB-20">
         <h4 class="c-grey-900 mB-20">
-            Mailchimp Subscribers
+            Mailchimp Абонати
             <button type="button" class="add-btn btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-form-type="add" data-form="subscribe">
               Добави
             </button>
@@ -52,6 +52,8 @@
         <thead>
           <tr>
             <th scope="col">Email</th> 
+            <th scope="col">Статус</th> 
+            <th scope="col">Действия</th> 
           </tr>
         </thead>
         <tbody>
