9b07f57c01bae52c017e35da28a9720e2ff73356
criting <galabin@rubberduck.xyz>
2019-02-15 16:07:32 +0200

    Fixing currency modal title

--- resources/views/admin/settings/currencies.blade.php
+++ resources/views/admin/settings/currencies.blade.php
@@ -7,7 +7,7 @@ aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="addCurrencyLabel">Добавяне на магазин</h5>
+                <h5 class="modal-title" id="addCurrencyLabel">Добавяне на валута</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
