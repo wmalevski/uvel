edf913dd464c4866e6b5d92ed0de7b6962d27887
Borislav Minchev <borislav.minchev@rubberduck.xyz>
2019-03-01 12:27:51 +0000

    fixed design for blogs page and single blog page

--- resources/views/store/pages/blog/single.blade.php
+++ resources/views/store/pages/blog/single.blade.php
@@ -3,13 +3,11 @@
 @section('content')
 <div id="content-wrapper-parent">
 	<div id="content-wrapper">
-		<!-- Content -->
 		<div id="content" class="clearfix">
 			<div id="breadcrumb" class="breadcrumb">
 				<div itemprop="breadcrumb" class="container">
 					<div class="row">
 						<div class="col-md-24">
-							{{-- {{ Breadcrumbs::render('single_translated_article', $article) }} --}}
 							{{ Breadcrumbs::render('single_translated_article', $article) }}
 						</div>
 					</div>
@@ -18,15 +16,6 @@
 			<section class="content">
 				<div class="container">
 					<div class="row">
-						<!--
-						{{--
-						<div id="page-header" class="col-md-24">
-							<h1 id="page-title" class="large">
-								{{ $article->translate($lng)->title }}
-							</h1>
-						</div>
-						--}}
-						-->
 						<div id="col-main" class="blog article-page col-xs-24 col-sm-24 ">
 							<div class="article">
 								<article class="blogs-item">
