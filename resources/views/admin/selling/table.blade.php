9e69f9ca68c126c19ecae9cfdc6c405b46d40f61
criting <galabin@rubberduck.xyz>
2019-04-11 13:58:42 +0300

    Changing structure of selling item for the FE calculations

--- resources/views/admin/selling/table.blade.php
+++ resources/views/admin/selling/table.blade.php
@@ -1,14 +1,3 @@
-{{-- <tr @if($item->attributes->type == 'product') data-saleProduct @endif>
-    <td>{{ $item->name }} {{ $item->id }}</th>
-    <td data-quantity>{{ $item->quantity }}</td>
-    <td data-weight="{{ $item->attributes->weight }}">{{ $item->attributes->weight }}</td>
-    <td>{{ $item->price }}</td>
-    <td style="display: none; visibility: hidden;" data-carat="{{ $item->attributes->carat }}">{{ $item->attributes->carat }}</td>
-    <td><span data-url="/ajax/sell/removeItem/{{$item->id}}" class="delete-btn cart"><i class="c-brown-500 ti-trash"></i></span></td>
-    @if($item->attributes->order != '')
-        <td style="display: none; visibility: hidden;" data-order-id="{{ $item->attributes->order }}"></td>
-    @endif
-</tr> --}}
 <tr @if($item->attributes->type == 'product') data-saleProduct data-type="{{ $item->attributes->mat }}" @endif>
     <td data-default-sample="{{ $item->attributes->defCode }}" data-sample="{{ $item->attributes->code }}" data-carat="{{ $item->attributes->carat }}">{{ $item->name }}</th>
     <td data-quantity>{{ $item->quantity }}</td>
