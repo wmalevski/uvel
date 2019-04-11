2d61bea9a770d28a1075e462e4824af45e8c40d0
criting <galabin@rubberduck.xyz>
2019-04-11 14:48:36 +0300

    Item order attribute

--- resources/views/admin/selling/table.blade.php
+++ resources/views/admin/selling/table.blade.php
@@ -4,4 +4,7 @@
     <td data-weight="{{ $item->attributes->weight }}">{{ $item->attributes->weight }}</td>
     <td>{{ $item->price }}</td>
     <td><span data-url="/ajax/sell/removeItem/{{$item->id}}" class="delete-btn cart"><i class="c-brown-500 ti-trash"></i></span></td>
+    @if($item->attributes->order != '')
+        <td style="display: none; visibility: hidden;" data-order-id="{{ $item->attributes->order }}"></td>
+    @endif
  </tr>
