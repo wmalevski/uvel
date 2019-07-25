fab4364780bcdde72e01fb0a01e73151b7a6d2bf
Tsvetelina <tsvetelina.panayotova@rubberduck.xyz>
2019-07-25 10:16:22 +0300

    UJS-449: Home page last products.

--- resources/views/store/pages/index.blade.php
+++ resources/views/store/pages/index.blade.php
@@ -212,103 +212,104 @@
 					@endif --}}
 					Нови продукти -->
 
-					@foreach($materials as $material)
-					<div class="home-feature">
-						<div class="container">
-							<div class="group_featured_products row">
-								<div class="col-md-24">
-									<div class="home_fp">
-										<h6 class="general-title">Последни от {{ $material->parent->name }}</h6>
-										<div class="home_fp_wrapper">
-											<div class="home_fp2">
+					@foreach($materials as $collect)
+						<div class="home-feature">
+							<div class="container">
+								<div class="group_featured_products row">
+									<div class="col-md-24">
+										<div class="home_fp">
+											<h6 class="general-title">Последни от {{ $collect->first()->parent->name }}</h6>
+											<div class="home_fp_wrapper">
+												<div class="home_fp2">
+													@foreach($collect as $material)
+														@foreach ( $material->productsOnline->take(10) as $key => $product )
+															@if (Illuminate\Support\Str::lower($product->store_info->name) != 'склад')
+															<li class="element no_full_width" data-alpha="{{ $product->name }}" data-price="{{ $product->price }}" data-id="{{$product->id}}">
+																<ul class="row-container list-unstyled clearfix">
 
-												@foreach ( $material->productsOnline->take(10) as $key => $product )
-													@if (Illuminate\Support\Str::lower($product->store_info->name) != 'склад')
-													<li class="element no_full_width" data-alpha="{{ $product->name }}" data-price="{{ $product->price }}" data-id="{{$product->id}}">
-														<ul class="row-container list-unstyled clearfix">
+																	<li class="row-left">
+																		<a href="{{ route('single_product', ['product' => $product->id])  }}" class="container_item">
+																			<img  class="img-fill" class="img-zoom img-responsive image-fly" alt="{{ $product->name }}" src="
+																			@if(count($product->photos))
+																			{{ asset("uploads/products/" . $product->photos->first()['photo']) }}
+																			@elseif(count($product->model->photos))
+																			{{ asset("uploads/models/" . $product->model->photos->first()['photo']) }}
+																			@else
+																			{{ asset('store/images/demo_375x375.png') }}
+																			@endif">
+																		</a>
+																		<div class="hbw">
+																			<span class="hoverBorderWrapper"></span>
+																		</div>
+																	</li>
 
-															<li class="row-left">
-																<a href="{{ route('single_product', ['product' => $product->id])  }}" class="container_item">
-																	<img  class="img-fill" class="img-zoom img-responsive image-fly" alt="{{ $product->name }}" src="
-																	@if(count($product->photos))
-																	{{ asset("uploads/products/" . $product->photos->first()['photo']) }}
-																	@elseif(count($product->model->photos))
-																	{{ asset("uploads/models/" . $product->model->photos->first()['photo']) }}
-																	@else
-																	{{ asset('store/images/demo_375x375.png') }}
-																	@endif">
-																</a>
-																<div class="hbw">
-																	<span class="hoverBorderWrapper"></span>
-																</div>
-															</li>
+																	<li class="row-right parent-fly animMix">
 
-															<li class="row-right parent-fly animMix">
-
-																<div class="product-content-left">
-																	<a class="title-5" href="{{ route('single_product', ['product' => $product->id]) }}">
-																		No: {{ implode(" ", str_split($product->id, 3)) }}
-																	</a>
-																	<br>
-																	Модел: {{ $product->model->name }}
-																	<br>
-																	{{ $product->material->name }} - {{ $product->material->code }} - {{ $product->material->color }}
-																	<br>
-																	{{ $product->weight['weight'] }}гр.
-																	<br>
-																	@if (isset($product->weight['stone']))
-																		{{ $product->weight['stone'] }}кт.
-																		<br>
-																	@endif
-																	Налично в: {{ $product->store_info->name }}
-																	<span class="spr-badge" data-rating="0.0">
-																	<span class="spr-starrating spr-badge-starrating">
-																		{{$product->listProductAvgRatingStars($product)}}
-																	</span>
-																</span>
-																</div>
-
-																<div class="product-content-right">
-																	<div class="product-price">
-																		<span class="price">
-																			{{ number_format($product->price) }} лв.
+																		<div class="product-content-left">
+																			<a class="title-5" href="{{ route('single_product', ['product' => $product->id]) }}">
+																				No: {{ implode(" ", str_split($product->id, 3)) }}
+																			</a>
+																			<br>
+																			Модел: {{ $product->model->name }}
+																			<br>
+																			{{ $product->material->name }} - {{ $product->material->code }} - {{ $product->material->color }}
+																			<br>
+																			{{ $product->weight['weight'] }}гр.
+																			<br>
+																			@if (isset($product->weight['stone']))
+																				{{ $product->weight['stone'] }}кт.
+																				<br>
+																			@endif
+																			Налично в: {{ $product->store_info->name }}
+																			<span class="spr-badge" data-rating="0.0">
+																			<span class="spr-starrating spr-badge-starrating">
+																				{{$product->listProductAvgRatingStars($product)}}
+																			</span>
 																		</span>
-																	</div>
-																</div>
+																		</div>
 
-																<div class="hover-appear">
-																	<a href="{{ route('single_product', ['product' => $product->id]) }}" class="effect-ajax-cart product-ajax-qs" title="Преглед">
-																		<input name="quantity" value="1" type="hidden">
-																		<i class="fa fa-lg fa-th-list"></i>
-																		<span class="list-mode">Преглед</span>
-																	</a>
+																		<div class="product-content-right">
+																			<div class="product-price">
+																				<span class="price">
+																					{{ number_format($product->price) }} лв.
+																				</span>
+																			</div>
+																		</div>
 
-																	<button class="product-ajax-qs hidden-xs hidden-sm quick_shop" data-target="#quick-shop-modal" data-toggle="modal"
-																		 data-url="products/{{ $product->id }}/" title="Бърз Преглед">
-																		<i class="fa fa-lg fa-eye"></i>
-																		<span class="list-mode">Бърз преглед</span>
-																	</button>
+																		<div class="hover-appear">
+																			<a href="{{ route('single_product', ['product' => $product->id]) }}" class="effect-ajax-cart product-ajax-qs" title="Преглед">
+																				<input name="quantity" value="1" type="hidden">
+																				<i class="fa fa-lg fa-th-list"></i>
+																				<span class="list-mode">Преглед</span>
+																			</a>
 
-																	<button class="wish-list" title="Добави в желани"
-																		 data-url="{{ route('wishlists_store', ['type' => 'product', 'item' => $product->id]) }}">
-																		<i class="fa fa-lg fa-heart"></i>
-																		<span class="list-mode">Добави в желани</span>
-																	</button>
-																</div>
+																			<button class="product-ajax-qs hidden-xs hidden-sm quick_shop" data-target="#quick-shop-modal" data-toggle="modal"
+																				 data-url="products/{{ $product->id }}/" title="Бърз Преглед">
+																				<i class="fa fa-lg fa-eye"></i>
+																				<span class="list-mode">Бърз преглед</span>
+																			</button>
 
+																			<button class="wish-list" title="Добави в желани"
+																				 data-url="{{ route('wishlists_store', ['type' => 'product', 'item' => $product->id]) }}">
+																				<i class="fa fa-lg fa-heart"></i>
+																				<span class="list-mode">Добави в желани</span>
+																			</button>
+																		</div>
+
+																	</li>
+																</ul>
 															</li>
-														</ul>
-													</li>
-													@endif
-												@endforeach
-
+															@endif
+														@endforeach
+													@endforeach
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
+
 					@endforeach
 
 					@if(count($articles))
