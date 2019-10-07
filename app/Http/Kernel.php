3aadce50034c241914bb56bfb2dc830e835d0338
Tsvetelina <tsvetelina.panayotova@rubberduck.xyz>
2019-10-07 09:29:49 +0300

    UJS-391: Removed items from cart after cookie timeout.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
+        \App\Http\Middleware\SessionTimeout::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
     ];
@@ -34,6 +35,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\SessionTimeout::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
