dbc691c671226427ba25a2070b863972d6b477e4
Tsvetelina <tsvetelina.panayotova@rubberduck.xyz>
2019-10-29 16:20:53 +0200

    Fixed stones in models.

--- resources/views/admin/models/table.blade.php
+++ resources/views/admin/models/table.blade.php
@@ -1,52 +1,53 @@
 <tr data-id="{{ $model->id }}">
-	<td class="thumbnail--tooltip">
-		@if(count($model->photos))
-			<img class="admin-product-image" src="{{ asset("uploads/models/" . $model->photos->first()['photo']) }}">
-			<ul  class="product-hover-image" style="background-image: url({{ asset("uploads/models/" . $model->photos->first()['photo']) }});" ></ul>
-		@endif
-	</td>
-	<td>{{ $model->name }}</td>
-	<td>
-		{{ $model->weight }}
-	</td>
-	<td>
-		{{ $model->price }}лв.
-	</td>
-	<td>
-		{{ $model->workmanship }}лв.
-	</td>
-	<td>
-		<span data-url="models/{{$model->id}}" class="edit-btn" data-form-type="edit" data-form="models" data-toggle="modal"
-		 data-target="#editModel">
+    <td class="thumbnail--tooltip">
+        @if(count($model->photos))
+            <img class="admin-product-image" src="{{ asset("uploads/models/" . $model->photos->first()['photo']) }}">
+            <ul class="product-hover-image"
+                style="background-image: url({{ asset("uploads/models/" . $model->photos->first()['photo']) }});"></ul>
+        @endif
+    </td>
+    <td>{{ $model->name }}</td>
+    <td>
+        {{ $model->weight }}
+    </td>
+    <td>
+        {{ $model->price }}лв.
+    </td>
+    <td>
+        {{ $model->workmanship }}лв.
+    </td>
+    <td>
+		<span data-url="models/{{$model->id}}" class="edit-btn" data-form-type="edit" data-form="models"
+              data-toggle="modal"
+              data-target="#editModel">
 			 <i class="c-brown-500 ti-pencil"></i>
 		</span>
-		<span data-url="models/delete/{{$model->id}}" class="delete-btn">
+        <span data-url="models/delete/{{$model->id}}" class="delete-btn">
 			<i class="c-brown-500 ti-trash"></i>
 		</span>
-	</td>
+    </td>
 
-	<td>
-		@if(!empty($model->stones->first()))
-			{{ App\StoneContour::where('id', App\Stone::where('id', $model->stones->first()->stone_id)->first()->contour_id)->first()->name }}
-			- {{ App\StoneSize::where('id', App\Stone::where('id', $model->stones->first()->stone_id)->first()->size_id)->first()->name }}
-		@endif
-	</td>
+    <td>
+        @if(!empty($model->stones->first()))
+            {{ App\StoneContour::where('id', App\Stone::where('id', $model->stones->first()->stone_id)->first()->contour_id)->first()->name }}
+            - {{ App\StoneSize::where('id', App\Stone::where('id', $model->stones->first()->stone_id)->first()->size_id)->first()->name }}
+        @else
+            Няма налични камъни.
+        @endif
+    </td>
 
-	<td class="stones--tooltip">
-		Виж камъни
-		<ul>
-			@if(!empty($model->stones->first()))
-				@foreach($model->stones as $stone)
-				<li>
-					Име: {{$stone->stone->nomenclature->name }} - {{$stone->stone->contour->name }} - {{$stone->stone->size->name }} , Количество: {{ $stone->amount }}
-				</li>
-				@endforeach
-			@else
-			<li>
-				Няма налични камъни.
-			</li>
-			@endif
-		</ul>
-	</td>
+    <td class="stones--tooltip">
+        @if(!empty($model->stones->first()))
+            Виж камъни
+            <ul>
+                @foreach($model->stones as $stone)
+                    <li>
+                        Име: {{$stone->stone->nomenclature->name }} - {{$stone->stone->contour->name }}
+                        - {{$stone->stone->size->name }} , Количество: {{ $stone->amount }}
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </td>
 </tr>
 
