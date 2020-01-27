e29b92256e10a9018f95c6afe3c858fd3fc00eaf
Tsvetelina <tsvetelina.panayotova@rubberduck.xyz>
2020-01-27 09:29:06 +0200

    UJS-507: Lable template and seed file.

--- resources/views/pdf/label.blade.php
+++ resources/views/pdf/label.blade.php
@@ -1,15 +1,22 @@
 <div style="clear: both; font-family: sans-serif;">
-    <div style="width: 33%; float: left;">
+    <div style="box-sizing: border-box; width: 30%; float: left; padding-right: 10px; font-size: 8px; text-align: center;">
         {{ $material->name }} - {{ $material->code }} - {{ $material->color }}
-        {!! str_replace( '<?xml version="1.0" standalone="no"?>', '' ,DNS1D::getBarcodeSVG('1234567', "EAN8",1,32,"black", true)) !!}
+        <div style="margin: 2px 0">{!! str_replace( '<?xml version="1.0" standalone="no"?>', '' ,DNS1D::getBarcodeSVG($barcode, "EAN13",0.9,14,"black", false)) !!}</div>
+        {{ $barcode }}
     </div>
     <div style="width: 30%; float: left; font-size: 12px;">
-        Гр: {{ $weight }} <br/>
-        @if(isset($stone['stone']))
-           {{ $stone[0]}} <br/>
-        @endif
-        Р-р: {{ $product->size }} <br/>
-        Изр: {{ $workmanship }}лв. <br/>
-        {{ $product->price }}лв.
+        <table>
+            <tr>
+                <td text-rotate="-90" style="font-size: 20px;">{{ $product->price }}лв.</td>
+                <td style="padding-left: 10px;">
+                    Гр: {{ $weight }} <br/>
+                    @if(isset($stone['stone']))
+                        {{ $stone[0]}} <br/>
+                    @endif
+                    Р-р: {{ $product->size }} <br/>
+                    Изр: {{ $workmanship }}лв. <br/>
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
