741eddd4f16964ae6abed2b2a2cfbe8b085a52fb
Velizar Malevski <velizar@droxic.com>
2020-04-10 15:55:43 +0300

    Fix.

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -914,10 +914,10 @@
                 @yield('content')
             </div>
         </main>
-        <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
+        <!-- <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
         <span>Copyright © 2017 Designed by
           <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span>
-        </footer>
+        </footer> -->
     </div>
 </div>
 <form id="logout-form" action="{{ route('admin_logout') }}" method="POST" style="display: none;">
