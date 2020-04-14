ffb525215c384e93d2955d9fdb768e3bbfbe6b07
Velizar Malevski <velizar@droxic.com>
2020-04-14 12:23:20 +0300

    Adding custom orders ID

--- resources/views/admin/orders/custom/index.blade.php
+++ resources/views/admin/orders/custom/index.blade.php
@@ -20,6 +20,7 @@ aria-hidden="true">
       <table id="main_table" class="table tablesort table-fixed">
         <thead>
           <tr>
+            <th data-sort-method="none">Уникален номер</th>
             <th scope="col">Снимка</th> 
             <th scope="col">Email</th> 
             <th scope="col">Телефон</th> 
@@ -31,12 +32,17 @@ aria-hidden="true">
           </tr>
 
           <tr class="search-inputs" data-dynamic-search-url="ajax/search/orders/custom">
+            <th></th>
+            <th></th>
             <th>
               <input class="filter-input form-control" type="text" data-dynamic-search-param="byEmail=" placeholder="Имейл">
             </th>
             <th></th>
             <th></th>
             <th></th>
+            @if(\Illuminate\Support\Facades\Auth::user()->role == 'admin')
+              <th></th>
+            @endif
           </tr>
         </thead>
         <tbody>
