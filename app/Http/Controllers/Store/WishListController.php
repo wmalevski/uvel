471cccf32a81cf7d361d4e60371db49f4f37a81d
Dave <dave.sarkisyan@rubberduck.xyz>
2021-02-01 08:58:20 +0200

    Fixing wishlist removal

--- app/Http/Controllers/Store/WishListController.php
+++ app/Http/Controllers/Store/WishListController.php
@@ -70,7 +70,7 @@ class WishListController extends BaseController{
 	public function destroy(WishList $wishList){
 		if($wishList){
 			$wishList->delete();
-			return Redirect::back()->with('success.wishlist', 'Продуктът беше успешно премахнат от списъка.');
+			return Redirect::back()->with('success', 'Продуктът беше успешно премахнат от списъка.');
 		}
 	}
 }
