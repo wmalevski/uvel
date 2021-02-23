3c4ff93426709cee64fa399fe0ef7acf08ceb4d7
Dave <dave.sarkisyan@rubberduck.xyz>
2021-02-23 14:51:36 +0200

    Fixing Expenses table

--- resources/views/admin/expenses/table.blade.php
+++ resources/views/admin/expenses/table.blade.php
@@ -3,7 +3,13 @@
     <td>{{ $expense->amount }}</td>
     <td>{{ $expense->store_from_id ? $expense->store_from_id : 'Няма данни' }}</td>
     <td>{{ $expense->store_to_id ? $expense->store_to_id : 'Няма данни' }}</td>
-    <td>{{ $expense->currency->name }}</td>
+    <td>
+        @if($expense->currency_id)
+        {{$expense->currency}}
+        @else
+        {{$currency::where('default', 'yes')->first()->name}}
+        @endif
+    </td>
     <td>{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $expense->created_at)->format('H:i d/m/Y') }}</td>
     <td>{{ $expense->additional_info }}</td>
     @if(\Illuminate\Support\Facades\Auth::user()->role == 'admin')
