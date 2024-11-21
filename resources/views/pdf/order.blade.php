f7f3a86406f93c21e4bb25bdeeca4e479647aae8
wmalevski <velizar@rubberduck.xyz>
2024-11-21 16:49:32 +0200

    Fixing pdfs

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -71,7 +71,7 @@
         <hr style="margin:5px 0 10px 0;clear: both;">
 
         <div style="float:left;width:50%;font-size:12px;"><b>За получаване: {{$order->date_returned}}</b></div>
-        <div style="float:left;width:50%;padding-top:5px;">{{ $barcode }}</div>
+        <div style="float:left;width:50%;padding-top:5px;">{!! $barcode !!}</div>
 
     </div>
 </div>
