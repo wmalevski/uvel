c27746d336d077b67bfd5cbaf8fd5cbe9e6a2807
wmalevski <velizar@rubberduck.xyz>
2024-11-21 17:26:54 +0200

    Fixing label display

--- resources/views/pdf/label.blade.php
+++ resources/views/pdf/label.blade.php
@@ -2,7 +2,6 @@
     <div style="box-sizing: border-box; width: 20%; float: left; padding-right: 10px; font-size: 8px; text-align: center;">
         {{ $material->name }} - {{ $material->code }}
         <div style="margin: 2px 0">{!! $barcodeSVG !!}</div>
-        {{ $barcode }}
     </div>
     <div style="width: 25%; float: left;">
         <table>
