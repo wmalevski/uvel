1b80f7ced9be06ea3952e514a994e31478f24c88
martin Duchev <martin.duchev@rubberduck.xyz>
2024-11-29 16:13:20 +0200

    Bug fix - fixed exception on ProductTravellingController::store() execution

--- app/Http/Controllers/ProductTravellingController.php
+++ app/Http/Controllers/ProductTravellingController.php
@@ -16,155 +16,161 @@ use Auth;
 use App\Setting;
 
 class ProductTravellingController extends Controller{
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index(){
-		// $products = Product::where('status', 'available')->take(env('SELECT_PRELOADED'))->orderBy('updated_at', 'DESC')->withTrashed()->get();
-		$loggedUser = Auth::user();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+        // $products = Product::where('status', 'available')->take(env('SELECT_PRELOADED'))->orderBy('updated_at', 'DESC')->withTrashed()->get();
+        $loggedUser = Auth::user();
 
-		$travelling = new ProductTravelling;
-		if(in_array($loggedUser->getRoles(), array('admin','storehouse'))){
-			$travelling = $travelling::with(['product', 'storeFrom', 'storeTo'])->orderBy('updated_at', 'DESC')->paginate(Setting::where('key','per_page')->first()->value ?? 30);
-		}
-		else{
-			$travelling = $travelling::with(['product', 'storeFrom', 'storeTo'])->whereRaw('store_from_id='.$loggedUser->store_id.' OR store_to_id = '.$loggedUser->store_id)->orderBy('updated_at', 'DESC')->paginate(Setting::where('key','per_page')->first()->value ?? 30);
-		}
-		// $stores = Store::all();
-		$userStoreId = $loggedUser->getStore()->id;
+        $travelling = new ProductTravelling;
+        if(in_array($loggedUser->getRoles(), array('admin','storehouse'))){
+            $travelling = $travelling::with(['product', 'storeFrom', 'storeTo'])->orderBy('updated_at', 'DESC')->paginate(Setting::where('key','per_page')->first()->value ?? 30);
+        }
+        else{
+            $travelling = $travelling::with(['product', 'storeFrom', 'storeTo'])->whereRaw('store_from_id='.$loggedUser->store_id.' OR store_to_id = '.$loggedUser->store_id)->orderBy('updated_at', 'DESC')->paginate(Setting::where('key','per_page')->first()->value ?? 30);
+        }
+        // $stores = Store::all();
+        $userStoreId = $loggedUser->getStore()->id;
 
-		return \View::make('admin/products_travelling/index', array(
-			'loggedUser'=>$loggedUser,
-			// 'products'=>$products,
-			'travelling'=>$travelling,
-			// 'stores'=>$stores,
-			'userStoreId' => $userStoreId,
-		));
-	}
+        return \View::make('admin/products_travelling/index', array(
+            'loggedUser'=>$loggedUser,
+            // 'products'=>$products,
+            'travelling'=>$travelling,
+            // 'stores'=>$stores,
+            'userStoreId' => $userStoreId,
+        ));
+    }
 
 
-	public function productstravellingReport(){
-		$products_travellings = ProductTravelling::orderBy('id','DESC')->where('date_received', '!=', 'NULL')->paginate(Setting::where('key','per_page')->first()->value ?? 30);
-		return view('admin.reports.productstravelling_reports.index', compact(['products_travellings']));
-	}
+    public function productstravellingReport(){
+        $products_travellings = ProductTravelling::orderBy('id','DESC')->where('date_received', '!=', 'NULL')->paginate(Setting::where('key','per_page')->first()->value ?? 30);
+        return view('admin.reports.productstravelling_reports.index', compact(['products_travellings']));
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request){
-		$validator = Validator::make($request->all(), array('store_to_id'=>'required'));
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), array('store_to_id'=>'required'));
 
-		if($validator->fails()){
-			return Response::json(array('errors'=>$validator->getMessageBag()->toArray()), 401);
-		}
+        if($validator->fails()){
+            return Response::json(array('errors'=>$validator->getMessageBag()->toArray()), 401);
+        }
 
-		$response = '';
-		if(!$request->product_id){
-			return Response::json(array('errors'=>array('quantity'=>array(trans('admin/products_travelling.error_no_products')))), 401);
-		}
-		foreach($request->product_id as $product){
-			$check = Product::find($product);
+        $response = '';
+        if(!$request->product_id){
+            return Response::json(array('errors'=>array('quantity'=>array(trans('admin/products_travelling.error_no_products')))), 401);
+        }
+        foreach($request->product_id as $product){
+            $check = Product::find($product);
 
-			if(!$check){
-				return Response::json(array('errors'=>array('not_found'=>array(trans('admin/products_travelling.error_not_found')))),401);
-			}
-			if($check->store_id == $request->store_to_id){
-				return Response::json(array('errors'=>array('quantity'=>array(trans('admin/products_travelling.error_same_store')))), 401);
-			}
+            if(!$check){
+                return Response::json(array('errors'=>array('not_found'=>array(trans('admin/products_travelling.error_not_found')))),401);
+            }
+            if($check->store_id == $request->store_to_id){
+                return Response::json(array('errors'=>array('quantity'=>array(trans('admin/products_travelling.error_same_store')))), 401);
+            }
 
-			$travel = new ProductTravelling();
-			$travel->product_id = $product;
-			$travel->store_from_id = $check->store_id;
-			$travel->store_to_id  = $request->store_to_id;
-			$travel->date_sent = new \DateTime();
-			$travel->user_sent = Auth::user()->getId();
+            $travel = new ProductTravelling();
+            $travel->product_id = $product;
+            $travel->store_from_id = $check->store_id;
+            $travel->store_to_id  = $request->store_to_id;
+            $travel->date_sent = new \DateTime();
+            $travel->user_sent = Auth::user()->getId();
 
-			$travel->save();
+            $travel->save();
 
-			$product = Product::find($product);
-			$product->store_id = 1;
-			$product->status = 'travelling';
-			$product->save();
+            $product = Product::find($product);
+            $product->store_id = 1;
+            $product->status = 'travelling';
+            $product->save();
 
-			$response .=  View::make('admin/products_travelling/table', array('product' => $travel, 'proID' => $travel->id))->render();
-		}
+            $loggedUser = Auth::user();
+            $userStoreId = $loggedUser->getStore()->id;
+            $response .=  View::make('admin/products_travelling/table', array(
+                'item' => $travel,
+                'proID' => $travel->id,
+                'userStoreId' => $userStoreId
+            ))->render();
+        }
 
-		return Response::json(array('success' => $response));
-	}
+        return Response::json(array('success' => $response));
+    }
 
-	public function addByScan($product){
-		$item = Product::where('barcode', $product)->first();
+    public function addByScan($product){
+        $item = Product::where('barcode', $product)->first();
 
-		if(!$item){
-			return Response::json(['errors' => ['not_found' => ['Продукта не може да бъде намерен.']]], 401);
-		}
-		$pass_item = (object)[
-			'id' => $item->id,
-			'name' => $item->id,
-			'weight' => $item->weight,
-			'barcode' => $item->barcode
-		];
+        if(!$item){
+            return Response::json(['errors' => ['not_found' => ['Продукта не може да бъде намерен.']]], 401);
+        }
+        $pass_item = (object)[
+            'id' => $item->id,
+            'name' => $item->id,
+            'weight' => $item->weight,
+            'barcode' => $item->barcode
+        ];
 
-		return Response::json(array('item' => $pass_item));
-	}
+        return Response::json(array('item' => $pass_item));
+    }
 
-	public function accept($product){
-		$existing_product = Product::where('barcode', $product)->first();
+    public function accept($product){
+        $existing_product = Product::where('barcode', $product)->first();
 
-		if(!$existing_product){
-			return Response::json(['errors' => ['not_found' => ['Продукта не може да бъде намерен.']]], 401);
-		}
+        if(!$existing_product){
+            return Response::json(['errors' => ['not_found' => ['Продукта не може да бъде намерен.']]], 401);
+        }
 
-		$travel = ProductTravelling::where('product_id',  $existing_product->id)->orderBy('id','DESC')->first();
+        $travel = ProductTravelling::where('product_id',  $existing_product->id)->orderBy('id','DESC')->first();
 
-		if($travel->status=="0"){
-			if(in_array(Auth::user()->role, array("admin", "manager"))
-				||
-				Auth::user()->store_id==$travel->store_to_id
-			){
-				$travel->status = '1';
-				$travel->user_received = Auth::user()->id;
-				$travel->date_received = new \DateTime();
-				$travel->save();
+        if($travel->status=="0"){
+            if(in_array(Auth::user()->role, array("admin", "manager"))
+                ||
+                Auth::user()->store_id==$travel->store_to_id
+            ){
+                $travel->status = '1';
+                $travel->user_received = Auth::user()->id;
+                $travel->date_received = new \DateTime();
+                $travel->save();
 
-				$existing_product->store_id = $travel->store_to_id;
-				$existing_product->status = 'available';
-				$existing_product->save();
+                $existing_product->store_id = $travel->store_to_id;
+                $existing_product->status = 'available';
+                $existing_product->save();
 
-				$response = View::make('admin/products_travelling/table', array('product' => $travel))->render();
+                $response = View::make('admin/products_travelling/table', array('product' => $travel))->render();
 
-				return Response::json(array(
-					'ID' => $travel->id,
-					'table' => $response,
-					'success' => 'Продукта бе успешно приет!'
-				), 200);
-			}
-			else{
-				return Response::json(['errors' => ['not_found' => ['Продукта не може да бъде намерен като пътуващ към Вашия магазин.']]], 401);
-			}
-		}
-	}
+                return Response::json(array(
+                    'ID' => $travel->id,
+                    'table' => $response,
+                    'success' => 'Продукта бе успешно приет!'
+                ), 200);
+            }
+            else{
+                return Response::json(['errors' => ['not_found' => ['Продукта не може да бъде намерен като пътуващ към Вашия магазин.']]], 401);
+            }
+        }
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  \App\ProductTravelling  $productTravelling
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy(ProductTravelling $product){
-		if($product){
-			$originProduct = Product::find($product->product_id);
-			$originProduct->store_id = $product->store_from_id;
-			$originProduct->status = 'available';
-			$originProduct->save();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductTravelling  $productTravelling
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductTravelling $product){
+        if($product){
+            $originProduct = Product::find($product->product_id);
+            $originProduct->store_id = $product->store_from_id;
+            $originProduct->status = 'available';
+            $originProduct->save();
 
-			$product->delete();
-			return Response::json(array('success' => 'Успешно изтрито!'));
-		}
-	}
+            $product->delete();
+            return Response::json(array('success' => 'Успешно изтрито!'));
+        }
+    }
 }
