2e3c535a361e0ac4103bad2ce88e1ffd455edb65
wmalevski <velizar@rubberduck.xyz>
2024-12-13 11:07:55 +0200

    fixing admin prices

--- resources/views/admin/selling/online/edit.blade.php
+++ resources/views/admin/selling/online/edit.blade.php
@@ -104,7 +104,7 @@
 
                 @if(!empty($discount_codes))
                   @foreach($discount_codes as $barcode=>$value)
-                    / <b>{{ ($product->price - ($value/100) )}} лв.</b> след отстъпка
+                    / <b>{{ ($product->price - ($product->price*($value/100) ) )}} лв.</b> след отстъпка
                     @break
                   @endforeach
                 </div>
