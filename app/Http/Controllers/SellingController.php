9977f5b1491392223cd5413e1cc19386d80ada38
wmalevski <velizar@rubberduck.xyz>
2024-12-18 11:30:38 +0200

    Fixes

--- app/Http/Controllers/SellingController.php
+++ app/Http/Controllers/SellingController.php
@@ -779,7 +779,7 @@ class SellingController extends Controller{
             }
         }
 
-        if(isset($product) && $type == 'product'){
+        if(isset($product) && $type !== true) {
             $mpdf = new \Mpdf\Mpdf([
                 'mode' => 'utf-8',
                 'format' => [148, 210],
--- resources/views/pdf/receipt_order_by_model.blade.php
+++ resources/views/pdf/receipt_order_by_model.blade.php
@@ -42,7 +42,7 @@
 <div style="text-align:right;"><b>Капаро</b>: {{$order->earnest ?: 0}}лв.</div>
 <div style="text-align:right;"><b>Обменен материал</b>: {{$exchange_material_sum}}лв.</div>
 <div style="text-align:right;"><b>Дадена сума</b>: {{$orderPayment->given}} лв.</div>
-<div style="text-align:right;"><b>Ресто</b>: {{ floatval( ($exchange_material_sum + $orderPayment->given) + $order->earnest - $selling->price )}}лв.</div>
+<div style="text-align:right;"><b>Ресто</b>: {{ floatval( ($exchange_material_sum + $orderPayment->given) + ($order->earnest ?? 0) - $selling->price )}}лв.</div>
 
 <div style="clear: both;height:20px;"></div>
 
