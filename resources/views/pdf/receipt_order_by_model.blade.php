d82ae52a27e92d9c327d8d847b405e14788c86e4
wmalevski <velizar@rubberduck.xyz>
2024-12-18 11:47:21 +0200

    Fixes

--- resources/views/pdf/receipt_order_by_model.blade.php
+++ resources/views/pdf/receipt_order_by_model.blade.php
@@ -42,7 +42,12 @@
 <div style="text-align:right;"><b>Капаро</b>: {{$order->earnest ?: 0}}лв.</div>
 <div style="text-align:right;"><b>Обменен материал</b>: {{$exchange_material_sum}}лв.</div>
 <div style="text-align:right;"><b>Дадена сума</b>: {{$orderPayment->given}} лв.</div>
-@dd($exchange_material_sum, $orderPayment->given, ($order->earnest ?? 0), $selling->price,( ($exchange_material_sum + $orderPayment->given) + ($order->earnest ?? 0) - $selling->price ), floatval( ($exchange_material_sum + $orderPayment->given) + ($order->earnest ?? 0) - $selling->price ));
+
+@php
+  $change = number_format( floatval($exchange_material_sum + $orderPayment->given) + floatval($order->earnest ?? 0) - floatval($selling->price), 2, '.', '' );
+@endphp
+
+@dd($exchange_material_sum, $orderPayment->given, ($order->earnest ?? 0), $selling->price,( ($exchange_material_sum + $orderPayment->given) + ($order->earnest ?? 0) - $selling->price ), floatval( ($exchange_material_sum + $orderPayment->given) + ($order->earnest ?? 0) - $selling->price ), $change);
 <div style="text-align:right;"><b>Ресто</b>: {{ floatval( ($exchange_material_sum + $orderPayment->given) + ($order->earnest ?? 0) - $selling->price )}}лв.</div>
 
 <div style="clear: both;height:20px;"></div>
