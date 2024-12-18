949cc576199c1b7b8a828ef2621545ff96bd2cf3
wmalevski <velizar@rubberduck.xyz>
2024-12-18 11:55:18 +0200

    Fixing file

--- app/Http/Controllers/SellingController.php
+++ app/Http/Controllers/SellingController.php
@@ -779,7 +779,7 @@ class SellingController extends Controller{
             }
         }
 
-        if(isset($product) && $type !== true) {
+        if(isset($product) && $product !== true) {
             $mpdf = new \Mpdf\Mpdf([
                 'mode' => 'utf-8',
                 'format' => [148, 210],
