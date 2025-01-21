aaf3e17a827f65053c20c0de51eb75d6db9df193
wmalevski <velizar@rubberduck.xyz>
2025-01-21 15:15:16 +0200

    Adding google tag manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <!-- Google tag (gtag.js) --> 
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-H4WMSSC58R"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-H4WMSSC58R'); </script>
 </head>
 <body>
     <div id="app">
